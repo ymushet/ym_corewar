338689da08c293d060a4ea69573bd7157a6312b4
opariy <alexandra_pariy@ukr.net>
2017-10-20 16:13:14 +0300

    fork lfork aff sub add zjump

--- commands/commands.h
+++ commands/commands.h
@@ -23,11 +23,12 @@ void ft_xor(t_process *process);
 void ft_zjmp(t_process *process);
 void ft_ldi(t_process *process);
 void ft_sti(t_process *process);
-void ft_fork(t_process *process);
 void ft_lld(t_process *process);
 void ft_lldi(t_process *process);
-void ft_lfork(t_process *process);
 void ft_aff(t_process *process);
 void ft_zjump(t_process *process);
 void	ft_add_sub(t_process *process, char sign);
+void 	ft_aff(t_process *process);
+void	ft_lfork_fork(t_process *process, char l);
+void	ft_zjump(t_process *process);
 #endif
---
+++ commands/ft_zjump.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_zjump.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opariy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/20 15:59:32 by opariy            #+#    #+#             */
+/*   Updated: 2017/10/20 15:59:34 by opariy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../corewar.h"
+
+void	ft_zjump(t_process *process)
+{
+	ft_increment_index(process);
+	if (process->cary == 1)
+	{
+		process->args[1] = (short)(g_dt.map[0][ft_increment_index(process)]
+				<< 8) | g_dt.map[0][ft_increment_index(process)];
+		process->mem_addres = ft_get_value(process->mem_addres - 3 + process->args[1] % IDX_MOD);
+		ft_bzero(process->args, 16);
+	}
+	else
+		process->mem_addres = ft_get_value(process->mem_addres + 2);
+}
+
